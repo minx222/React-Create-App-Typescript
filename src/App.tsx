c901d1c88dbdca01ec54fac102b52544255f7ddb
莫才学 <1969954161@qq.com>
2023-02-24 21:33:10 +0800

    aaa

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Provider } from 'react-redux'
-<<<<<<< HEAD
 import stores from '@/stores';
 import AppRouter from '@/routers';
 const App: React.FC = ()=> 
@@ -8,14 +7,3 @@ const App: React.FC = ()=>
   </Provider>
 
 export default App;
-=======
-import stores from '@/stores'
-import AppRouter from '@/routers'
-const App: React.FC = () => (
-  <Provider store={stores}>
-    <AppRouter />
-  </Provider>
-)
-
-export default App
->>>>>>> 4a7b81305d2cc1585d8cb16488b48df677ad39c5
